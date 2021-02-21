a1317b2e001dc5a51ef70d055e5f70967385c75d
denchik <cherepanov.denis1994@gmail.com>
2021-02-21 21:37:56 +0300

    add bottom navigation

--- app/src/main/java/com/example/petproject/MainActivity.kt
+++ app/src/main/java/com/example/petproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.petproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petproject.app.CardManagementRepo
 import com.example.petproject.app.TestClassA
@@ -11,6 +12,7 @@ import com.example.petproject.datasource.LocalDataSource
 import com.example.petproject.di.ApplicationComponent
 import com.example.petproject.di.DaggerApplicationComponent
 import com.example.petproject.login.ui.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -33,6 +35,32 @@ class MainActivity : AppCompatActivity() {
         start_btn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_1 -> {
+                    Toast.makeText(this, "first", Toast.LENGTH_SHORT)
+                    // navigate
+                    true
+                }
+                R.id.page_2 -> {
+                    Toast.makeText(this, "second", Toast.LENGTH_SHORT)
+                    // navigate
+                    true
+                }
+                R.id.page_3 -> {
+                    Toast.makeText(this, "third", Toast.LENGTH_SHORT)
+                    // navigate
+                    true
+                }
+                R.id.page_4 -> {
+                    Toast.makeText(this, "fourth", Toast.LENGTH_SHORT)
+                    // navigate
+                    true
+                }
+                else -> false
+            }
         }
 
         testClassA.show()
