6a5510622f2c1a24258340a77560deca4eb0306f
d.cherepanov <d.cherepanov@cardsmobile.ru>
2021-06-10 16:04:53 +0300

    add bottom Nav manager

--- app/src/main/java/com/example/petproject/MainActivity.kt
+++ app/src/main/java/com/example/petproject/MainActivity.kt
@@ -2,29 +2,42 @@ package com.example.petproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.petproject.util.BottomNavManager
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var bottomNavView: BottomNavigationView
-
-//    private val backStack: ArrayList<Int> = arrayListOf()
+    private lateinit var bottomNavManager: BottomNavManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavView = findViewById(R.id.bottom_nav_view)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        if (savedInstanceState == null) {
+            setupNavigationManager()
+        }
+    }
 
-        bottomNavView.setupWithNavController(navController)
+    private fun setupNavigationManager() {
+        bottomNavManager = BottomNavManager(
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.container_fragment,
+            bottomNavigationView = findViewById(R.id.bottom_nav_view)
+        )
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        bottomNavManager.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        bottomNavManager.onRestoreInstanceState(savedInstanceState)
+        setupNavigationManager()
+    }
+
+    override fun onBackPressed() {
+        if (!bottomNavManager.onBackPressed()) super.onBackPressed()
     }
 
 }
